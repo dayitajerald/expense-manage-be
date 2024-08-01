5f4721c731159e12b90c0be76b53426b8a963f83
dayitajerald <dayita.jerald@gmail.com>
2024-08-01 08:54:06 +0530

    percentage change added

--- src/main/java/com/expense/app/controller/IncomeController.java
+++ src/main/java/com/expense/app/controller/IncomeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:5173")
+@CrossOrigin("http://localhost:517")
 @RequestMapping("/incomes")
 public class IncomeController {
     @Autowired
